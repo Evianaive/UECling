1299d1d98832b8ca8f162e7a7e64ab07c7434614
Evianaive <153540933@qq.com>
2025-01-04 19:41:45 +0800

    Add Module To Hold Script

---
+++ Source/ClingScript/ClingScript.Build.cs
@@ -0,0 +1,26 @@
+﻿using UnrealBuildTool;
+
+public class ClingScript : ModuleRules
+{
+    public ClingScript(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.NoPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore"
+            }
+        );
+    }
+}
---
+++ Source/ClingScript/Private/ClingScript.cpp
@@ -0,0 +1,17 @@
+﻿#include "ClingScript.h"
+
+#define LOCTEXT_NAMESPACE "FClingScriptModule"
+
+void FClingScriptModule::StartupModule()
+{
+    
+}
+
+void FClingScriptModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FClingScriptModule, ClingScript)
