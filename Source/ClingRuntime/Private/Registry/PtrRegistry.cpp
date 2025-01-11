484ed687692fd8b749a206f74ead192fd2ec8f5a
Evianaive <153540933@qq.com>
2025-01-11 20:35:46 +0800

    Add a ptr registry class to let access easier in cling console

---
+++ Source/ClingRuntime/Private/Registry/PtrRegistry.cpp
@@ -0,0 +1,16 @@
+﻿
+#include "Registry/PtrRegistry.h"
+
+TMap<FName, void*> FPtrRegistry::PtrMap;
+
+void* FPtrRegistry::Get(const FName& Name)
+{
+	if(auto Re = PtrMap.Find(Name))
+		return *Re;
+	return nullptr;
+}
+
+void FPtrRegistry::Register(const FName& Name, void* Ptr)
+{
+	PtrMap.Add(Name,Ptr);
+}
---
+++ Source/ClingRuntime/Public/Registry/PtrRegistry.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class CLINGRUNTIME_API FPtrRegistry
+{
+public:
+	static void* Get(const FName& Name);
+	template<typename T, typename TStr>
+	static T* Get(TStr&& Str)
+	{
+		return static_cast<T*>(Get(Forward(Str)));
+	}
+	
+	static void Register(const FName& Name, void* Ptr);
+	template<typename T, typename TStr>
+	static void Register(TStr&& Name, T* Ptr)
+	{
+		Register(Forward(Name),Ptr);
+	}
+private:
+	static TMap<FName, void*> PtrMap;
+};
